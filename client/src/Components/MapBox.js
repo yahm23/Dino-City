8e43c0f46299d0338530351af31ee239daab8b79
Marcin <marcinszydelko@yahoo.com>
2020-01-31 18:37:32 +0000

    add MapBox and MapTileRow components

---
+++ client/src/Components/MapBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapTileRow from './MapTileRow';
+
+function MapBox({children}) {
+        return(
+            <>
+                <h2>Map Box</h2>
+                <MapTileRow >
+
+                </MapTileRow>
+            </>
+        )
+}
+export default MapBox;
---
+++ client/src/Components/MapTileRow.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class MapTileRow extends Component {
+    render(props) {
+        return(
+           <p>tilerow</p>
+        )
+    }
+}
+export default MapTileRow;
