fc08117f7785698d62bfaaaa21cce4395acb3a76
Marcin <marcinszydelko@yahoo.com>
2020-01-31 12:07:08 +0000

    initial commit

---
+++ dino-city-server/src/main/java/com/triceratops/dinocityserver/DinoCityServerApplication.java
@@ -0,0 +1,13 @@
+package com.triceratops.dinocityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoCityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoCityServerApplication.class, args);
+	}
+
+}
---
+++ dino-city-server/src/test/java/com/triceratops/dinocityserver/DinoCityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triceratops.dinocityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoCityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
