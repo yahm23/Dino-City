8129d02a8b38cc268674d42aabf47d9983eaba65
Marcin <marcinszydelko@yahoo.com>
2020-02-02 15:12:01 +0000

    fix total in Enclosure

--- client/src/Components/Enclosure.js
+++ client/src/Components/Enclosure.js
@@ -10,22 +10,21 @@ function Enclosure({money}) {
 
     const [size, setSize] = useState({size: "SMALL", price:2000});
     const [security, setSecurity] = useState({security: "LOW", price: 1.2});
-    const [total, setTotal] = useState(2400);
-
 
     const handleSizeChange = (e) => {
-        setSize({size: e.target.id, price: e.target.value});
-        setTotal(totalCost());
+        setSize({size: e.target.id, price: parseInt(e.target.value)});
     }
 
     const handleSecurityChange = (e) => {
-        setSecurity({security: e.target.id, price:e.target.value});
-        setTotal(totalCost());
-    
+        setSecurity({security: e.target.id, price: parseFloat(e.target.value)});   
     }
 
     function totalCost() {
-        return (parseInt(size.price * security.price) + size.price);
+        return (security.price * size.price);
+    }
+
+    function handleBuy() {
+        
     }
 
    
@@ -37,24 +36,53 @@ function Enclosure({money}) {
                 <Row>
                     <Col>
                         <div className="size-panel">
-                            <Form.Check type="radio" name="size" id="SMALL" value={2000} default onChange={handleSizeChange} label="Small"/>
-                            <Form.Check type="radio" name="size" id="MEDIUM" value={5000} onChange={handleSizeChange} label="Medium"/>
-                            <Form.Check type="radio" name="size" id="LARGE" value={12000} onChange={handleSizeChange} label="Large"/>
+                            <Form.Check type="radio" name="size" id="SMALL" value={2000} 
+                                checked={size.price === 2000 } 
+                                onChange={handleSizeChange} 
+                                label="Small"
+                            />
+                            <Form.Check type="radio" name="size" id="MEDIUM" value={5000} 
+                                checked={size.price === 5000 } 
+                                onChange={handleSizeChange} 
+                                label="Medium"
+                            />
+                            <Form.Check type="radio" name="size" id="LARGE" value={12000} 
+                                checked={size.price === 12000 } 
+                                onChange={handleSizeChange} 
+                                label="Large"
+                            />
                         </div>
                     </Col>
                     <Col>
                         <div className="security-panel">
-                            <Form.Check type="radio" name="security" id="LOW" value={1.2} default onChange={handleSecurityChange} label="Low"/>
-                            <Form.Check type="radio" name="security" id="MEDIUM" value={1.4} onChange={handleSecurityChange} label="Medium"/>
-                            <Form.Check type="radio" name="security" id="HIGH" value={1.6} onChange={handleSecurityChange} label="High"/>
+                            <Form.Check type="radio" name="security" id="LOW" value={1.2} 
+                                checked={security.price === 1.2 } 
+                                onChange={handleSecurityChange} 
+                                label="Low"
+                            />
+                            <Form.Check type="radio" name="security" id="MEDIUM" value={1.4} 
+                                checked={security.price === 1.4 } 
+                                onChange={handleSecurityChange} 
+                                label="Medium"
+                            />
+                            <Form.Check type="radio" name="security" id="HIGH" value={1.6} 
+                                checked={security.price === 1.6 } 
+                                onChange={handleSecurityChange} 
+                                label="High"
+                            />
                         </div>
                     </Col>
                 </Row>
                 <Row>
-                    <p>Total: €{total}</p>
+                    <p>Total: €{totalCost()}</p>
                 </Row>
                 <Row>
-                <Button variant={money >= total ? "success" : "secondary"} disabled={total > money} >Buy</Button>
+                    <Button 
+                    variant={money >= totalCost() ? "success" : "secondary"} 
+                    disabled={totalCost() > money} 
+                    onClick={handleBuy} >
+                        Buy
+                    </Button>
                 </Row>
             </Container>
         )
