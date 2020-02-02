51f8458034f4b31620d95e9aae9ec781b72c90f8
Marcin <marcinszydelko@yahoo.com>
2020-02-02 02:12:37 +0000

    add radio buttons

--- client/src/Components/Enclosure.js
+++ client/src/Components/Enclosure.js
@@ -9,29 +9,33 @@ function Enclosure() {
 
     const handleSizeChange = (e) => {
         setSize({size: e.target.id, price: e.target.value});
+        total();
     }
 
     const handleSecurityChange = (e) => {
         setSecurity({security: e.target.id, price:e.target.value});
+        total();
     }
 
-    
+    function total() {
+        return (size.price * security.price + size.price);
+    }
 
 
    
         return(
             <>
                 <h1>Build Enclosure</h1>
-                <Form.Check type="radio" name="size" id="SMALL" value={2000} default onChange={handleSizeChange}/> Small<br/>
+                <Form.Check type="radio" name="size" id="SMALL" value={2000}  onChange={handleSizeChange}/> Small<br/>
                 <Form.Check type="radio" name="size" id="MEDIUM" value={5000} onChange={handleSizeChange}/> Medium<br/>
                 <Form.Check type="radio" name="size" id="LARGE" value={12000} onChange={handleSizeChange}/> Large  
 
-                <Form.Check type="radio" name="security" id="LOW" value={1.2} default onChange={handleSecurityChange}/> Low<br/>
-                <Form.Check type="radio" name="security" id="MEDIUM" value="1.4" onChange={handleSecurityChange}/> Medium<br/>
-                <Form.Check type="radio" name="security" id="HIGH" value="1.6" onChange={handleSecurityChange}/> High 
+                <Form.Check type="radio" name="security" id="LOW" value={1.2}  onChange={handleSecurityChange}/> Low<br/>
+                <Form.Check type="radio" name="security" id="MEDIUM" value={1.4} onChange={handleSecurityChange}/> Medium<br/>
+                <Form.Check type="radio" name="security" id="HIGH" value={1.6} onChange={handleSecurityChange}/> High 
             
 
-                <p>Total: €{size.price + (size.price * security.price)}</p>
+                <p>Total: €{total()}</p>
             </>
         )
     
