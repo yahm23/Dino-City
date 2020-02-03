541324fef35bf9dd772ec0677eb983d1ade123c5
Marcin <marcinszydelko@yahoo.com>
2020-02-03 16:27:40 +0000

    restructure Enum DinoType

--- dino-city-server/src/main/java/com/triceratops/dinocityserver/models/enums/DinoType.java
+++ dino-city-server/src/main/java/com/triceratops/dinocityserver/models/enums/DinoType.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum DinoType {
-    TYRANNOSAURUS("Tyrannosaurus",20, DietType.CARNIVORE, 3000.0, ThreatLevel.HIGH),
+    TYRANNOSAURUS("Tyrannosaurus", 20, DietType.CARNIVORE, 3000.0, ThreatLevel.HIGH),
     ALBERTOSAURUS("Albertosaurus", 15, DietType.CARNIVORE, 2500.0, ThreatLevel.HIGH),
     CARNOTAURUS("Carnotaurus", 15, DietType.CARNIVORE, 2200.0, ThreatLevel.HIGH),
     BARYONYX("Baryonyx", 10, DietType.CARNIVORE, 1500.0, ThreatLevel.MEDIUM),
     MEGALOSAURUS("Megalosaurus", 10, DietType.CARNIVORE, 1700.0, ThreatLevel.MEDIUM),
     VELOCIRAPTOR("Velociraptor", 5, DietType.CARNIVORE, 1000.0, ThreatLevel.MEDIUM),
     DEINONYCHUS("Deinonychus", 6, DietType.CARNIVORE, 1200.0, ThreatLevel.MEDIUM),
-    COMPSOGNATHUS("Compsognathus",2, DietType.CARNIVORE, 300.0, ThreatLevel.MEDIUM),
+    COMPSOGNATHUS("Compsognathus", 2, DietType.CARNIVORE, 300.0, ThreatLevel.MEDIUM),
     BRACHIOSAURUS("Brachiosaurus", 20, DietType.HERBIVORE, 3000.0, ThreatLevel.LOW),
     IGUANODON("Iguanodon", 20, DietType.HERBIVORE, 2500.0, ThreatLevel.MEDIUM),
     TRICERATOPS("Triceratops", 18, DietType.HERBIVORE, 2200.0, ThreatLevel.MEDIUM),
@@ -19,19 +19,21 @@ public enum DinoType {
     EDMONTOSAURUS("Edmontosaurus", 15, DietType.HERBIVORE, 2000.0, ThreatLevel.MEDIUM),
     STEGOSAURUS("Stegosaurus", 15, DietType.HERBIVORE, 1800.0, ThreatLevel.MEDIUM),
     STEGOCERAS("Stegoceras", 10, DietType.HERBIVORE, 1500.0, ThreatLevel.LOW),
-    HOMALOCEPHALE("Homalocephale", 4, DietType.HERBIVORE, 1000.0, ThreatLevel.LOW );
+    HOMALOCEPHALE("Homalocephale", 4, DietType.HERBIVORE, 1000.0, ThreatLevel.LOW);
 
 
-    public String name;
-    public int size;
-    private ThreatLevel threatLevel = null;
+    private String name;
+    private int size;
+    private DietType dietType;
     private double price;
+    private ThreatLevel threatLevel;
 
-    DinoType(String name, int size, DietType diet, double price, ThreatLevel threatLevel) {
+    DinoType(String name, int size, DietType dietType, double price, ThreatLevel threatLevel) {
         this.name = name;
-        this.size =size;
+        this.size = size;
+        this.dietType = dietType;
+        this.price = price;
         this.threatLevel = threatLevel;
-        this.price=price;
     }
 
     public String getName() {
@@ -42,11 +44,16 @@ public enum DinoType {
         return size;
     }
 
-    public ThreatLevel getThreatLevel() {
-        return threatLevel;
+    public DietType getDietType() {
+        return dietType;
     }
 
     public double getPrice() {
         return price;
     }
+
+    public ThreatLevel getThreatLevel() {
+        return threatLevel;
+    }
 }
+
