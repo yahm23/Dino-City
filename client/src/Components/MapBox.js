1b07613544d9628d31b05fa480d986530ae58aa7
EismanW <pl.amosso@gmail.com>
2020-02-04 18:38:46 +0000

    Cleaned components

--- client/src/Components/MapBox.js
+++ client/src/Components/MapBox.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import MapTileRow from './MapTileRow';
-import MapTile from './MapTile';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 
-
-
-function MapBox({children}) {
-
-        const path = [
-            '/tiles/grass_01.jpg',
-            '/tiles/grass_02.jpg',
-            '/tiles/grass_03.jpg',
-            '/tiles/grass_04.jpg',
-            '/tiles/grass_05.jpg',
-            '/tiles/grass_06.jpg'
-        ]
-
-        const enclouser = '/enclousers/enclouser.png';
-
-        
-
+export default function MapBox({children}) {
         return(
             <Container>
                 {children}
             </Container>
         )
 }
-export default MapBox;
--- client/src/Components/MapTile.js
+++ client/src/Components/MapTile.js
@@ -2,20 +2,19 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Image from "react-bootstrap/Image";
 
-function MapTile({img, children}) {
+export default function MapTile({img, children}) {
 
 const mapImg = (selectedImg) => {
     return `/tiles/${selectedImg}.jpg`
-}
+};
 
     return(
         <Col>
         <div className="map-tile-wrapper">
-            <Image className="map-tile" src={mapImg(img)} fluid></Image>
+            <Image className="map-tile" src={mapImg(img)} fluid/>
             {children}
         </div>
         </Col>
     )
 
 }
-export default MapTile;
